225f5acf54793bc919f7aa3ede69354170986212
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-07-08 22:36:34 -0300

    learning basic stuff

---
+++ calculate_age.py
@@ -0,0 +1,6 @@
+birth_year = input('Birth year: ')
+age = 2019 - int(birth_year)
+print(age)
+
+# float()
+# bool()
---
+++ variables.py
@@ -0,0 +1,9 @@
+price = 10
+rating = 4.9
+name = 'Filipe'
+is_published = False #booleans started with capital letter
+print(price)
+
+name = 'John Smith'
+age = 20
+new_patient = True
